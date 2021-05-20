142c6a0fe14bf16e403c16f2a8b1bf52cb578bcb
Jossie Esteban Rodríguez Salazar <jerodriguez@merqueo.com>
2021-05-20 14:40:05 -0500

    services: implements moodle docker

--- services/wordpress/init/initdb.sql
+++ services/wordpress/init/initdb.sql
@@ -1,8 +1,11 @@
 -- create databases
 CREATE DATABASE IF NOT EXISTS `wordpress`;
+CREATE DATABASE IF NOT EXISTS `moodle`;
 
 -- create user
 CREATE USER IF NOT EXISTS `wordpress`@`%` IDENTIFIED BY 'VeZdF7Qd2E';
+CREATE USER IF NOT EXISTS `moodle`@`%` IDENTIFIED BY 'VeZdF7Qd2E';
 
 -- grant rights
 GRANT ALL PRIVILEGES ON wordpress.* TO `wordpress`@`%`;
+GRANT ALL PRIVILEGES ON moodle.* TO `moodle`@`%`;
